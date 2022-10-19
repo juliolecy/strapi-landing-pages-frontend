21790f0ea526c267693f5198bfd1c61ee55e4f00
jack <juliocesar_19@hotmail.com.br>
2022-10-19 12:33:05 -0300

    129th class

--- src/components/Dummy/Dummy.test.jsx
+++
@@ -1,9 +0,0 @@
-import { screen } from '@testing-library/react';
-import { renderTheme } from '../../styles/render-theme';
-
-describe('<Dummy/>', () => {
-  it('should render', () => {
-    renderTheme(<Dummy>Children</Dummy>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
-  });
-});
