334f969a6ff2c7da2c1940fa12f92be93b6400b0
JackLins <juliocesar_19@hotmail.com.br>
2022-10-23 02:08:15 -0300

    redirecting fetch api to heroku server

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://localhost:1337/api/pages/?filters[slug]=',
+  url: 'https://strapi-landing-pages-v4.herokuapp.com/api/pages/?filters[slug]=',
   siteName: 'Jack',
   defaultSlug: 'my-page',
 };
